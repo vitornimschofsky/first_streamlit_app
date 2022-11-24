302be50669b4786a5c1fe466dd8afe223fb65666
Vitor F de L Nimschofsky <89933194+vitornimschofsky@users.noreply.github.com>
2022-11-24 12:36:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ def get_fruit_load_list():
 if streamlit.button("Get Fruit Load List"):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      my_data_row = get_fruit_load_list()
+     my_cnx.close()
      streamlit.dataframe(my_data_row)
 
 
